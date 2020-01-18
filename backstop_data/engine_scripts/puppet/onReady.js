a4536be8569bbcf50870f8a54f2e9e1566b271f1
hitode909 <hitode909@gmail.com>
2020-01-18 14:44:36 +0900

    super hack to disable all transitions

--- backstop_data/engine_scripts/puppet/onReady.js
+++ backstop_data/engine_scripts/puppet/onReady.js
@@ -2,5 +2,16 @@ module.exports = async (page, scenario, vp) => {
   console.log('SCENARIO > ' + scenario.label);
   await require('./clickAndHoverHelper')(page, scenario);
 
+  // disable transition
+  await page.evaluate(() => {
+    const style = document.createElement('style');
+    style.innerHTML = `
+    * {
+      transition-duration: 0s !important;
+    }
+    `
+    document.body.append(style);
+  });
+
   // add more ready handlers here...
 };
